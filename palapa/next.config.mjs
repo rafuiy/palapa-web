3780051e32a8f72210beac60033d45d005e0dff8
Rafiputraaa <raafiputraa2@gmail.com>
2025-03-17 23:34:59 +0700

    fix: build

--- palapa/next.config.mjs
+++ palapa/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Jika menggunakan App Router (Next.js 13+)
-  },
     // output: "export",
     images: {
         unoptimized: true, // Agar gambar bisa di-load di Netlify tanpa optimasi Next.js
--- palapa/src/app/page.js
+++ palapa/src/app/page.js
@@ -17,6 +17,7 @@ import { LuHeartHandshake } from "react-icons/lu";
 import { PiRocketLaunch, PiCheckCircleFill  } from "react-icons/pi";
 
 import { useState } from "react";
+import { useEffect } from "react";
 import ModalAboutUs from "./components/ModalAboutUs";
 import ModalCooperation from "./components/ModalCooperation";
 import CardCarousel from "./components/Product";
@@ -27,40 +28,11 @@ import MarqueeButton from "./components/MoveButtons";
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
 
 export default function Home() {
   const [activeModal, setActiveModal] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isCooperationModalOpen, setIsCooperationModalOpen] = useState(false);
-
-  if (typeof document !== "undefined") {
-    const title = document.title; // ✅ Ini hanya berjalan di client
-  }
-  
+  const [isCooperationModalOpen, setIsCooperationModalOpen] = useState(false);  
 
   const modals = {
     wartelsus: {
@@ -82,6 +54,29 @@ export default function Home() {
         "Paypas (Payment Pemasyarakatan) adalah sistem pembayaran elektronik yang memanfaatkan virtual account dan teknologi biometrik untuk memastikan transaksi yang aman, efisien, dan terkontrol di lingkungan pemasyarakatan.",
     },
   };
+
+  useEffect(() => {
+    AOS.init({
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', 
+    });
+  }, []);
 
 
   return (
